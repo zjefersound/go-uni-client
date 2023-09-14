5d92c36468a041c462a0ed2784aa88f81a506fc1
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2023-09-14 17:52:24 -0300

    create Bill repository

--- src/services/bill.ts
+++ src/services/bill.ts
@@ -1,96 +1,32 @@
 import { getSessionUser } from "@/app/api/auth/[...nextauth]/functions/getSessionUser";
-import { sanityClient } from "@/configs/sanity";
-import { IBill, IBillPayload } from "@/models/IBill";
+import { IBillPayload } from "@/models/IBill";
 import { IServiceOptions } from "@/models/IServiceOptions";
-import { filtersToGroq } from "@/utils/filtersToGroq";
-import { toSanityRef } from "@/utils/toSanityRef";
-import { groq } from "next-sanity";
+import BillRepository from "@/repositories/BillRepository";
 
-const getAll: (options?: IServiceOptions) => Promise<IBill[]> = async ({
-  filters,
-} = {}) => {
+const getAll = async ({ filters = [] }: IServiceOptions = {}) => {
   const user = await getSessionUser();
-  return sanityClient.fetch(groq`*[_type == "bill" && receiver._ref == "${
-    user.id
-  }"] ${filtersToGroq(filters)} | order(date desc) {
-    _id,
-    payer -> {
-      _id,
-      name,
-      username,
-      avatar,
-    },
-    receiver -> {
-      _id,
-      name,
-      username,
-      avatar,
-    },
-    date,
-    amount,
-    currency,
-    description,
-    paid,
-  }`);
+  return BillRepository.getAll({
+    filters: [
+      { key: "receiver._id", operation: "==", value: user.id },
+      ...filters,
+    ],
+  });
 };
 
-const getById: (id: string) => Promise<IBill> = (id) => {
-  return sanityClient.fetch(groq`*[_type == "bill"][_id == "${id}"][0]{
-    _id,
-    payer -> {
-      _id,
-      name,
-      username,
-      avatar,
-    },
-    receiver -> {
-      _id,
-      name,
-      username,
-      avatar,
-    },
-    date,
-    amount,
-    currency,
-    description,
-    paid,
-  }`);
+const getById = (id: string) => {
+  return BillRepository.getById(id);
 };
 
 const create = (bill: IBillPayload) => {
-  const payload: any = {
-    _type: "bill",
-    date: bill.date,
-    amount: bill.amount,
-    currency: bill.currency,
-    description: bill.description,
-    paid: bill.paid,
-    receiver: toSanityRef(bill.receiverId),
-  };
-
-  if (bill.payerId) {
-    payload.payer = toSanityRef(bill.payerId);
-  }
-  return sanityClient.create(payload);
+  return BillRepository.create(bill);
 };
 
 const patch = (id: string, bill: IBillPayload) => {
-  const payload: any = { ...bill };
-
-  if (bill.receiverId) {
-    payload.receiver = toSanityRef(bill.receiverId);
-    delete payload.receiverId;
-  }
-  if (bill.payerId) {
-    payload.payer = toSanityRef(bill.payerId);
-    delete payload.payerId;
-  }
-
-  return sanityClient.patch(id).set(payload).commit();
+  return BillRepository.patch(id, bill);
 };
 
 const handleDelete = (id: string) => {
-  return sanityClient.delete(id);
+  return BillRepository.delete(id);
 };
 
 export const billService = {
